b655b4767f4c952768e6932b3f556b0791931be7
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2019-12-05 13:47:36 +0100

    Second Commit

--- Algorithms-And-Data-Structures.js
+++ Algorithms-And-Data-Structures.js
@@ -1,6 +1,6 @@
 console.log("up and running!");
 /*NOTES*/
 /*
-Better implementation means faster time and use less memory(RAM)
+Better implementation means faster time and use less memory(RAM) usage
 
 */
