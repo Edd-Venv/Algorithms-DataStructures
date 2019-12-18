56bf3842a8bdd6db93fc2ffd7a725301385300f8
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2019-12-18 19:28:47 +0100

    Updated Notes

--- Algorithms-With-Unit-Testing/chunk/index.js
+++ Algorithms-With-Unit-Testing/chunk/index.js
@@ -6,7 +6,42 @@
 // chunk(["a", "b", "c", "d"], 2) --> [[ "a", "b"], ["c", "d"]]
 // chunk([0, 1, 2, 3, 4, 5], 4) -->  [[0, 1, 2, 3], [4, 5]]
 
-function chunk(array, size) {}
+//Solution 1
+/*function chunk(array, size) {
+  "use strict";
+
+  let finalArray = [[], []];
+
+  for (let i = 0; i < size; i++) {
+    finalArray[0].push(array[i]);
+  }
+  for (let i = size; i < array.length; i++) {
+    finalArray[1].push(array[i]);
+  }
+  return finalArray;
+}
+//Time Complexity = O(N)
+//Space Complexity = O(N)
+*/
+
+//Solution 2
+/*
+function chunk(array, size) {
+  let res = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const item = array[i];
+    const last = res[res.length - 1];
+
+    if (!last || last.length === size) {
+      res.push([item]);
+    } else {
+      last.push(item);
+    }
+  }
+  return res;
+}
+*/
 
 mocha.setup("bdd");
 const { assert } = chai;
