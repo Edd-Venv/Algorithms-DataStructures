b7f766ee1c6aa1256fb0b36d6511eba1a63b78b3
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-01-07 08:43:09 +0100

    Updated Notes

--- Algorithms-With-Unit-Testing/sorting/index.js
+++ Algorithms-With-Unit-Testing/sorting/index.js
@@ -1,34 +1,25 @@
 // Implement a swap helper function that we will use in both BS and SS
-function swap(arr, i, j) {}
+function swap(arr, i, j) {
+  "use strict";
+  const valueI = arr[i];
+  const valueJ = arr[j];
+
+  for (let k = 0; k < arr.length; k++) {
+    if (k === i) {
+      arr[i] = valueJ;
+    } else if (k === j) {
+      arr[j] = valueI;
+    }
+  }
+
+  return arr;
+}
 
 //bubbleSort works by having sorted data accumulate at end of array
 function bubbleSort(arr) {}
 
 //selectionSort works by having sorted data accumulate at start of array
 function selectionSort(arr) {}
-
-// _________ _______  _______ _________   _______  _______  _______  _______  _______
-// \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
-//    ) (   | (    \/| (    \/   ) (     | (    \/| (   ) || (    \/| (    \/| (    \/
-//    | |   | (__    | (_____    | |     | |      | (___) || (_____ | (__    | (_____
-//    | |   |  __)   (_____  )   | |     | |      |  ___  |(_____  )|  __)   (_____  )
-//    | |   | (            ) |   | |     | |      | (   ) |      ) || (            ) |
-//    | |   | (____/\/\____) |   | |     | (____/\| )   ( |/\____) || (____/\/\____) |
-//    )_(   (_______/\_______)   )_(     (_______/|/     \|\_______)(_______/\_______)
-//                             ____       _
-//                             |  _ \     | |
-//                             | |_) | ___| | _____      __
-//                             |  _ < / _ \ |/ _ \ \ /\ / /
-//                             | |_) |  __/ | (_) \ V  V /
-//                             |____/ \___|_|\___/ \_/\_/
-//                         ______ ______ ______ ______ ______
-//                         |______|______|______|______|______|
-
-//                          ______ ______ ______ ______ ______
-//                         |______|______|______|______|______|
-
-//                          ______ ______ ______ ______ ______
-//                         |______|______|______|______|______|
 
 mocha.setup("bdd");
 const { assert } = chai;
