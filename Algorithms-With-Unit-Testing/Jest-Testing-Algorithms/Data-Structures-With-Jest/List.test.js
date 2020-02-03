3288e7725007b98d7034e032b48e440e9ec39364
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-02-03 16:13:53 +0100

    List CLEAR

--- Algorithms-With-Unit-Testing/Jest-Testing-Algorithms/Data-Structures-With-Jest/List.test.js
+++ Algorithms-With-Unit-Testing/Jest-Testing-Algorithms/Data-Structures-With-Jest/List.test.js
@@ -6,6 +6,7 @@ class List {
     this.listSize = 0;
     this.pos = 0;
     this.length = length;
+    //this.clear = this.clear;
     //this.append = append;
     /*this.clear = clear;
     this.find = find;
@@ -22,10 +23,14 @@ class List {
     this.getElment = getElment;
     this.contains = contains;*/
   }
+  clear() {
+    this.dataStore = [];
+    this.length = 0;
+    return true;
+  }
 
   append(data) {
-    this.dataStore[this.length] = data;
-    this.length++;
+    this.dataStore[this.length++] = data;
     return true;
   }
 }
@@ -34,11 +39,19 @@ describe("List Data Structure", () => {
   it("APPEND Should Add An Element To The DataStore", () => {
     const ListDS = new List();
     ListDS.append(5);
-    ListDS.append(7);
     ListDS.append(10);
     const length = ListDS.length - 1;
     const lastElement = ListDS.dataStore[length];
 
     expect(lastElement).toEqual(10);
+  });
+
+  it("CLEAR Should empty the List", () => {
+    const ListDS = new List();
+    ListDS.append(10);
+    ListDS.clear();
+
+    expect(ListDS.length).toBe(0);
+    expect(ListDS.dataStore).toEqual([]);
   });
 });
