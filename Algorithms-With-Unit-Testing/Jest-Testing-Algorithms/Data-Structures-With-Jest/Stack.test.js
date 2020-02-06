77a427aba7f57734288fb9437e82db10bf19ef07
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-02-06 18:07:31 +0100

    Stack

--- Algorithms-With-Unit-Testing/Jest-Testing-Algorithms/Data-Structures-With-Jest/Stack.test.js
+++ Algorithms-With-Unit-Testing/Jest-Testing-Algorithms/Data-Structures-With-Jest/Stack.test.js
@@ -5,10 +5,21 @@ class Stack {
     this.push;
     this.pop;
     this.peek;
+    this.length;
+  }
+
+  length() {
+    return this.top;
   }
 
   push(value) {
     this.dataStore[this.top++] = value;
+  }
+
+  shift() {
+    if (this.top >= 0) {
+      return this.dataStore[--this.top];
+    }
   }
 
   peek() {
@@ -19,16 +30,23 @@ class Stack {
   }
 
   pop() {
-    if (this.top < 1) {
-      return;
+    if (this.top < 2) {
+      console.log("here");
+      return this.shift();
     }
-    const newStack = new Stack();
+    if (this.top > 1) {
+      const poppedValue = this.dataStore[this.top - 1];
+      const newStack = new Stack();
 
-    for (let i = 0; i < this.top - 1; i++) {
-      newStack.push(this.dataStore[i]);
+      for (let i = 0; i < this.top - 1; i++) {
+        newStack.push(this.dataStore[i]);
+      }
+
+      this.top--;
+      this.dataStore = newStack.dataStore;
+
+      return poppedValue;
     }
-    this.top--;
-    return (this.dataStore = newStack.dataStore);
   }
 }
 
@@ -40,6 +58,15 @@ describe("Stack Data Structure", () => {
     StackDS.push(6);
 
     expect(StackDS.dataStore).toEqual([4, 5, 6]);
+  });
+
+  it("SHIFT", () => {
+    const StackDS = new Stack();
+    StackDS.push(1);
+    StackDS.push(2);
+    StackDS.push(3);
+
+    expect(StackDS.shift()).toBe(3);
   });
 
   it("PEEK It Should Return The Value At The Top Of The Stack", () => {
@@ -57,9 +84,40 @@ describe("Stack Data Structure", () => {
     StackDS.push(8);
     StackDS.push(9);
     StackDS.push(10);
-    StackDS.pop();
-    console.log(StackDS);
 
+    expect(StackDS.pop()).toBe(10);
     expect(StackDS.dataStore).toEqual([7, 8, 9]);
+  });
+
+  it.skip("Should Return Null If The Length Is 1", () => {
+    const StackDS = new Stack();
+    StackDS.push(11);
+    StackDS.pop();
+    expect(StackDS.dataStore).toEqual([11]);
+    //expect(StackDS.pop()).toBeNull();
+  });
+});
+
+// IMPLEMENTATION
+function reverse(word) {
+  let reverseStr = "";
+  const stack = new Stack();
+
+  for (let i = 0; i < word.length; i++) {
+    stack.push(word[i]);
+  }
+  for (let j = 0; j <= stack.top; j++) {
+    reverseStr += stack.pop();
+  }
+  if (word === reverseStr) {
+    return true;
+  }
+  console.log(reverseStr);
+  return false;
+}
+
+describe("STACK IMPLEMENTATION", () => {
+  it("Should Return True If Word Is A Palindrome.", () => {
+    expect(reverse("racecar")).toBeTruthy();
   });
 });
