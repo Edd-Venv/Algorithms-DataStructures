5ec479f401a02e81c9b72d94cfbaaac9d5ad9d20
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-02-13 16:51:12 +0100

    Queue

--- Algorithms-With-Unit-Testing/Jest-Testing-Algorithms/Data-Structures-With-Jest/Queue.test.js
+++ Algorithms-With-Unit-Testing/Jest-Testing-Algorithms/Data-Structures-With-Jest/Queue.test.js
@@ -1,6 +1,7 @@
 class Queue {
   constructor() {
     this.dataStore = [];
+    this.length;
     this.enqueue;
     this.dequeue;
     this.front;
@@ -8,20 +9,93 @@ class Queue {
     this.toString;
     this.empty;
   }
-  length() {
-    return this.dataStore.length;
-  }
+
   enqueue(element) {
     this.dataStore.push(element);
     return true;
   }
 
   dequeue() {
-    if (this.length > 0) {
-      this.dataStore.shift();
+    this.dataStore.shift();
+    return true;
+  }
+
+  front() {
+    this.dataStore[0];
+  }
+
+  back() {
+    this.dataStore[this.length - 1];
+  }
+
+  toString() {
+    let resultString = "";
+    for (let i = 0; i < this.dataStore.length; i++) {
+      resultString += "\n" + this.dataStore[i];
+    }
+    return resultString;
+  }
+
+  empty() {
+    if (this.dataStore.length === 0) {
       return true;
     }
     return false;
   }
-  front() {}
 }
+
+describe("QUEUE", () => {
+  it("Should Enqueue To The DataStore", () => {
+    const queue = new Queue();
+    queue.enqueue(2);
+    expect(queue.dataStore).toEqual([2]);
+  });
+  it("Should Dequeue The DataStore", () => {
+    const queue = new Queue();
+    queue.enqueue(1);
+    queue.enqueue(2);
+    queue.dequeue();
+    expect(queue.dataStore).toEqual([2]);
+  });
+  it("Should Return A List Of The DataStore", () => {
+    const queue = new Queue();
+    queue.enqueue(3);
+    queue.enqueue(4);
+    queue.enqueue(5);
+    expect(queue.toString()).toMatch("\n3\n4\n5");
+  });
+});
+//                                       IMPLEMENTATION 1
+
+class dancerObject {
+  constructor() {
+    this.dataStore = {};
+    this.length = length;
+  }
+  add(sex, name) {
+    this.dataStore[name] = sex;
+    this.length++;
+  }
+  length() {
+    return this.dataStore.length;
+  }
+}
+
+const tester = new dancerObject();
+tester.add("m", "frank");
+tester.add("m", "luke");
+tester.add("f", "frank");
+tester.add("f", "luke");
+console.log(tester.dataStore["frank"]);
+/*const dancer = (name, sex) => {
+  name = name.toLowerCase();
+  sex = sex.toLowerCase();
+
+  const dancerObj = new dancerObject();
+  dancerObj.add(name, sex);
+
+  return dancerObj.dataStore;
+};
+
+console.log(dancer("luke", "M"));
+*/
