f891fa5c8843bfbbfdc75a0b308df9a4e39cec51
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-02-25 11:34:41 +0100

    LinkedList

--- Algorithms-With-Unit-Testing/Jest-Testing-Algorithms/Data-Structures-With-Jest/LinkedList.test.js
+++ Algorithms-With-Unit-Testing/Jest-Testing-Algorithms/Data-Structures-With-Jest/LinkedList.test.js
@@ -13,5 +13,37 @@ class LinkedList {
     this.insert;
     this.remove;
     this.display;
+    this.unshift;
+  }
+
+  unshift(data) {
+    const newHead = new Node(data, this.head);
+    this.length++;
+    this.head = newHead;
+  }
+
+  find(item) {
+    let currentNode = this.head;
+    while (currentNode.data != item) {
+      currentNode = currentNode.next;
+    }
+    return currentNode;
+  }
+
+  insert(data, item) {
+    const current = this.find(item);
+    const newNode = new Node(data, current.next);
+    current.next = newNode;
+    this.length++;
   }
 }
+
+describe("LINKED LIST", () => {
+  it("UNSHIFT Should Add A Node At The begining Of The LinkedList", () => {
+    const llist = new LinkedList();
+    llist.unshift(1);
+    llist.unshift(2);
+    expect(llist).toHaveProperty("head.next.data", 1);
+    expect(llist).toHaveProperty("length", 2);
+  });
+});
