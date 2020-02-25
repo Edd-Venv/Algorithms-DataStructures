b9d4535430e83d7a375cc45f4e202ded9a31916c
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-02-25 09:32:42 +0100

    LINKED-LIST INIT

---
+++ Algorithms-With-Unit-Testing/Jest-Testing-Algorithms/Data-Structures-With-Jest/LinkedList.test.js
@@ -0,0 +1,17 @@
+class Node {
+  constructor(data, next) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+    this.find;
+    this.insert;
+    this.remove;
+    this.display;
+  }
+}
